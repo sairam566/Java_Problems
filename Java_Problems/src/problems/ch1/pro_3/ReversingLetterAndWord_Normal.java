8ce8708c61e429caf019674bcd4bf82e9a5d8e3a
sairam566 <sairam1431995@gmail.com>
2023-11-20 23:10:43 +0530

    Added Time Capturing capability

--- Java_Problems/src/problems/ch1/pro_3/ReversingLetterAndWord_Normal.java
+++ Java_Problems/src/problems/ch1/pro_3/ReversingLetterAndWord_Normal.java
@@ -1,5 +1,7 @@
 package problems.ch1.pro_3;
 
+import java.util.concurrent.TimeUnit;
+
 public class ReversingLetterAndWord_Normal {
 
 	private static final String WHITESPACE = " ";
@@ -8,14 +10,22 @@ public class ReversingLetterAndWord_Normal {
 		String paragraph = "reverse only the letters of each word";
 		StringBuilder builder = new StringBuilder();
 		String[] words = paragraph.split(WHITESPACE);
-		for(String word : words) {
-			for(int position=word.length()-1;position>=0;position--) {
+		long startTime = System.nanoTime();
+		for (String word : words) {
+			for (int position = word.length() - 1; position >= 0; position--) {
 				builder.append(word.charAt(position));
 			}
 			builder.append(WHITESPACE);
 		}
+
+		displayExecutionTime(System.nanoTime() - startTime);
 		System.out.println(builder.toString());
-		// esrever ylno eht srettel fo hcae drow 
+		// esrever ylno eht srettel fo hcae drow
+	}
+
+	public static void displayExecutionTime(long time) {
+		System.out.println("Execution time: " + time + " ns" + " ("
+				+ TimeUnit.MILLISECONDS.convert(time, TimeUnit.NANOSECONDS) + " ms)");
 	}
 
 }
