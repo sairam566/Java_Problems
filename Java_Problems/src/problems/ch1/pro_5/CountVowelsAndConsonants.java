399e3e86670b907c5b11e3a12e7c713847770fa4
sairam566 <sairam1431995@gmail.com>
2023-12-03 17:33:33 +0530

    Java 8

--- Java_Problems/src/problems/ch1/pro_5/CountVowelsAndConsonants.java
+++ Java_Problems/src/problems/ch1/pro_5/CountVowelsAndConsonants.java
@@ -9,10 +9,11 @@ public class CountVowelsAndConsonants {
 	private static final Set<Character> allVowels = new HashSet<Character>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
 
 	public static void main(String[] args) {
-		
+
 		String value = " Count Vowels & Consonants";
-		
-		System.out.println(countVowelsAndConsonants_V1(value));
+
+		//System.out.println(countVowelsAndConsonants_V1(value));
+		System.out.println(countVowelsAndConsonants_V2(value));
 	}
 
 	// Count Vowels and Consonants
@@ -29,5 +30,12 @@ public class CountVowelsAndConsonants {
 			}
 		}
 		return Pair.of(vowelsCount, consonantsCount);
+	}
+
+	// Count Vowels and Consonants Java-8
+	public static Pair<Long,Long> countVowelsAndConsonants_V2(String value){
+		Long vowelsCount = value.toLowerCase().chars().filter( c-> allVowels.contains((char)c)).count();
+		Long ConsonantsCount = value.toLowerCase().chars().filter(c-> !allVowels.contains((char)c)).filter(ch-> (ch>='a' && ch<='z')).count();
+		return Pair.of(vowelsCount, ConsonantsCount);
 	}
 }
